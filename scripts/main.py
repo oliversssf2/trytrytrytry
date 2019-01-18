c02e46e8f14340643fb2f366b9a256d78923acc1
oliversssf <40682820+oliversssf@users.noreply.github.com>
2019-01-18 19:08:43 +0800

    tranfoming the deprojecting and csv generating function from c++ to python

--- scripts/main.py
+++ scripts/main.py
@@ -1,20 +1,14 @@
-from library.ROBODK.robolink import *
-from library.ROBODK.robodk import *
 import pyrealsense2 as rs
 import cv2
 import numpy as np
-import array
-import csv
 
 
 pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-profile: rs.pipeline_profile = pipeline.start(config)
+cfg = pipeline.start()
+profile = cfg.get_stream(rs.stream.depth)
+intr = profile.as_video_stream_profile().get_intrinsics()
 
-
-depth_sensor: rs.sensor = profile.get_device().first_depth_sensor()
+depth_sensor: rs.sensor = cfg.get_device().first_depth_sensor()
 depth_scale = depth_sensor.get_depth_scale()
 
 align_to = rs.stream.color
@@ -23,8 +17,7 @@ align = rs.align(align_to)
 clipping_distance_in_meters = 1
 clipping_distance = clipping_distance_in_meters / depth_scale
 
-coord = array.array('f', [0,0,0])
-intr = profile.as_video_stream_profile().get_intrinsics()
+coord = [0,0,0]
 
 try:
     while True:
@@ -52,17 +45,25 @@ try:
 
         if key & 0xFF == ord('q') or key == 27: #SHUT DOWN THE TERMINAL WHILE 'Q' OR ESC IS PRESSED
             cv2.destroyAllWindows()
-        if key == ord('h'):
-            for i in (0, depth_frame.get_height(), 10):
-                for j in (0, depth_frame.get_width(), 10):
-                    depth = depth_frame.get_distance(i,j)
-                    if depth > 0:
-                        tem = array.array('f', [i,j])
-                        rs.rs2_deproject_pixel_to_point(coord, intr , tem, depth_frame)
-                        f = open("mycsv.csv", "x")
-                        f.write(coord[0] + ', ' + coord[1] + ',' + coord[2] + ',' + "0, 0, 180")
             break
-
+        if key == ord('h'):
+            f = open("mycsv.csv", 'w')
+            i = 0
+            j = 0
+            tem = [0, 0]
+            width = depth_frame.get_width()
+            height = depth_frame.get_height()
+            for i in range(0, width, 5):
+                for j in range(0, height, 5):
+                    depth = depth_frame.get_distance(i,j)
+                    if depth > 0 and depth < 0.5:
+                        coord = rs.rs2_deproject_pixel_to_point(intr, [i,j], depth)
+                        f.write(str(coord[0] * 1000))
+                        f.write(',')
+                        f.write(str(coord[1] * 1000))
+                        f.write(',')
+                        f.write(str(coord[2] * 1000))
+                        f.write('\n')
 finally:
     pipeline.stop()
     #rs.rs2_deproject_pixel_to_point()
